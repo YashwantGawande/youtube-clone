cd4e041945a45c1c26fd793a4f6d09bb8bef65cf
YashwantNG <gawandeyashwant7@gmail.com>
2023-01-27 00:27:17 +0530

    update

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoVideoUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link>
+        <CardMedia image={snippet?.thumbnails?.high?.url} />
+      </Link>
+    </Card>
+  );
+};
+
+export default VideoCard;
