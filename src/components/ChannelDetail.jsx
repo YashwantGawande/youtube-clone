753ea77ab72d19f0b00eff6a69b03058fdaa0b3f
YashwantNG <gawandeyashwant7@gmail.com>
2023-01-29 19:16:16 +0530

    update

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFormApi } from "../utils/FetchFromApi";
+import { videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
 
-export default ChannelDetail
+  const { id } = useParams;
+
+  useEffect(() => {
+    fetchFormApi('channels?part="snippets&id=${id}').then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+  }, [id]);
+
+  return <Box></Box>;
+};
+
+export default ChannelDetail;
